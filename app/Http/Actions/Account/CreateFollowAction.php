4aa95d36e2a9ef058e6f118f3ae1046d1f917eae
shinjo15 <shinjo0015@gmail.com>
2026-09-04 10:27:20 +0900

    fix: resolve account follow request

--- app/Http/Actions/Account/CreateFollowAction.php
+++ app/Http/Actions/Account/CreateFollowAction.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Actions\Account;
 
-use App\Http\Requests\Follow\CreateFollowRequest;
+use App\Http\Requests\Account\CreateFollowRequest;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 use RuntimeException;
